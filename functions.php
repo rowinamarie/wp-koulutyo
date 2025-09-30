1e3a0743a34c38d0e615987143a7e3cffc256765
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-09-30 15:00:17 +0300

    added comments

--- functions.php
+++ functions.php
@@ -7,9 +7,11 @@ function my_custom_theme_enqueue_styles() {
 add_action('wp_enqueue_scripts', 'my_custom_theme_enqueue_styles');
 // 'wp_enqueue_scripts' on hook, jota WordPress ajaa silloin, kun skriptejä ja tyylejä ladataan sivulle.
 
-Tällä rivillä siis sanotaan:
-➝ "Kun WordPress on lataamassa skriptejä ja tyylejä, aja myös my_custom_theme_enqueue_styles."
+//Tällä rivillä siis sanotaan:
+// "Kun WordPress on lataamassa skriptejä ja tyylejä, aja myös my_custom_theme_enqueue_styles."
 
+
+//tällä varmistetaan että teemalle rekisteröidään otsikkotuki heti alussa.
 function my_custom_theme_wp_title() {
     add_theme_support('title-tag');
 }

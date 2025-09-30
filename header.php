f01cd424a365dbd7b8deb2756477622f48f1d87d
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-09-30 15:30:38 +0300

    added logo functionalities

--- header.php
+++ header.php
@@ -1,13 +1,37 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 
-<html <?php language_attributes (); ?> >
+<html <?php language_attributes(); ?>>
 
 <head>
-    <meta name= "viewport" content= "width=device-width, initial-scale=1.0">
-    <meta charset="<?php bloginfo('charset') ?>" >
-    <?php wp_head (); ?> <!-- THis allows Wordpress and plugins insert element into the head section, such as stylesheets and scripts-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="<?php bloginfo('charset') ?>">
+    <?php wp_head(); ?>
+    <!-- THis allows Wordpress and plugins insert element into the head section, such as stylesheets and scripts-->
 
 </head>
 
 
 <body>
+
+    <header class="header-container">
+        <div class="my-logo">
+            <?php
+            if (function_exists('the_custom_logo' && has_custom_logo())) {
+                the_custom_logo();
+            } else {
+                //Fallback image
+                ?>
+                <a href="<?php echo home_url('/'); ?>">
+                    <img src="<?php echo get_template_directory_uri(); ?> /images/dummy-logo.png"
+                        alt="<?php bloginfo('name') ?>" width="100px">
+                </a>
+
+                <?php
+            }
+
+            ?>
+
+
+        </div>
+
+    </header>

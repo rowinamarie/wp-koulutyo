40fea1a686265697d2d6f736ce2c5aae79c7156e
Rowina San Juan <rowina.sanjuan@karhuhelsinki.fi>
2025-09-30 14:07:38 +0300

    added meta information

--- header.php
+++ header.php
@@ -3,7 +3,9 @@
 <html <?php language_attributes (); ?> >
 
 <head>
-
+    <meta name= "viewport" content= "width=device-width, initial-scale=1.0">
+    <meta charset="<?php bloginfo('charset') ?>" >
+    <?php wp_head (); ?> <!-- THis allows Wordpress and plugins insert element into the head section, such as stylesheets and scripts-->
 
 </head>
 
